2d51cc5bc44576f27cec0fa2cdaa6dced9955f4f
Gayan Malshan <agulugaha@outlook.com>
2019-05-08 15:44:03 +0530

    Create More Entities as asked

--- src/main/java/com/divudi/entity/NewProject.java
+++ src/main/java/com/divudi/entity/NewProject.java
@@ -17,8 +17,32 @@ public class NewProject {
     private Land land;
     private PPreliminaryActivities preliminaryActivities;
     private String projectActivities;
-    private Benificiaries benificiaries;
     private String rationaleOfProject;
+    private String objective1;
+    private String objective2;
+    private String objective3;
+    private String whatIsTheProblem;
+    private String whatAreTheRootCauses;
+    private String howThisFullFillExistingGap;
+    private String modeOfIntervention;
+    private Benificiaries benificiaries;
+    private String policyElementOfFrameWork;
+    private String contribution;
+    private String sectoralMasterPlan;
+    private String sectoralMasterPlanPriority;
+    private boolean isProjectIncludedToTheMasterPlan;
+    private String sectroStrategy;
+    private StakeHolders stakeHolders;
+    private Partners partners;
+    private String economicImapact;
+    private String enviromentImpact;
+    private String SocialImpact;
+    private ProjectOutcomes projectOutcomes;
+    private ProjectOutputs projectOutputs;
+    private Activity activity;
+    
+   
+    
     
 
     /**
@@ -131,6 +155,174 @@ public class NewProject {
      */
     public void setRationaleOfProject(String rationaleOfProject) {
         this.rationaleOfProject = rationaleOfProject;
+    }
+
+    public String getObjective1() {
+        return objective1;
+    }
+
+    public void setObjective1(String objective1) {
+        this.objective1 = objective1;
+    }
+
+    public String getObjective2() {
+        return objective2;
+    }
+
+    public void setObjective2(String objective2) {
+        this.objective2 = objective2;
+    }
+
+    public String getObjective3() {
+        return objective3;
+    }
+
+    public void setObjective3(String objective3) {
+        this.objective3 = objective3;
+    }
+
+    public String getWhatIsTheProblem() {
+        return whatIsTheProblem;
+    }
+
+    public void setWhatIsTheProblem(String whatIsTheProblem) {
+        this.whatIsTheProblem = whatIsTheProblem;
+    }
+
+    public String getWhatAreTheRootCauses() {
+        return whatAreTheRootCauses;
+    }
+
+    public void setWhatAreTheRootCauses(String whatAreTheRootCauses) {
+        this.whatAreTheRootCauses = whatAreTheRootCauses;
+    }
+
+    public String getHowThisFullFillExistingGap() {
+        return howThisFullFillExistingGap;
+    }
+
+    public void setHowThisFullFillExistingGap(String howThisFullFillExistingGap) {
+        this.howThisFullFillExistingGap = howThisFullFillExistingGap;
+    }
+
+    public String getModeOfIntervention() {
+        return modeOfIntervention;
+    }
+
+    public void setModeOfIntervention(String modeOfIntervention) {
+        this.modeOfIntervention = modeOfIntervention;
+    }
+
+    public String getPolicyElementOfFrameWork() {
+        return policyElementOfFrameWork;
+    }
+
+    public void setPolicyElementOfFrameWork(String policyElementOfFrameWork) {
+        this.policyElementOfFrameWork = policyElementOfFrameWork;
+    }
+
+    public String getContribution() {
+        return contribution;
+    }
+
+    public void setContribution(String contribution) {
+        this.contribution = contribution;
+    }
+
+    public String getSectoralMasterPlan() {
+        return sectoralMasterPlan;
+    }
+
+    public void setSectoralMasterPlan(String sectoralMasterPlan) {
+        this.sectoralMasterPlan = sectoralMasterPlan;
+    }
+
+    public String getSectoralMasterPlanPriority() {
+        return sectoralMasterPlanPriority;
+    }
+
+    public void setSectoralMasterPlanPriority(String sectoralMasterPlanPriority) {
+        this.sectoralMasterPlanPriority = sectoralMasterPlanPriority;
+    }
+
+    public boolean isIsProjectIncludedToTheMasterPlan() {
+        return isProjectIncludedToTheMasterPlan;
+    }
+
+    public void setIsProjectIncludedToTheMasterPlan(boolean isProjectIncludedToTheMasterPlan) {
+        this.isProjectIncludedToTheMasterPlan = isProjectIncludedToTheMasterPlan;
+    }
+
+    public String getSectroStrategy() {
+        return sectroStrategy;
+    }
+
+    public void setSectroStrategy(String sectroStrategy) {
+        this.sectroStrategy = sectroStrategy;
+    }
+
+    public StakeHolders getStakeHolders() {
+        return stakeHolders;
+    }
+
+    public void setStakeHolders(StakeHolders stakeHolders) {
+        this.stakeHolders = stakeHolders;
+    }
+
+    public Partners getPartners() {
+        return partners;
+    }
+
+    public void setPartners(Partners partners) {
+        this.partners = partners;
+    }
+
+    public String getEconomicImapact() {
+        return economicImapact;
+    }
+
+    public void setEconomicImapact(String economicImapact) {
+        this.economicImapact = economicImapact;
+    }
+
+    public String getEnviromentImpact() {
+        return enviromentImpact;
+    }
+
+    public void setEnviromentImpact(String enviromentImpact) {
+        this.enviromentImpact = enviromentImpact;
+    }
+
+    public String getSocialImpact() {
+        return SocialImpact;
+    }
+
+    public void setSocialImpact(String SocialImpact) {
+        this.SocialImpact = SocialImpact;
+    }
+
+    public ProjectOutcomes getProjectOutcomes() {
+        return projectOutcomes;
+    }
+
+    public void setProjectOutcomes(ProjectOutcomes projectOutcomes) {
+        this.projectOutcomes = projectOutcomes;
+    }
+
+    public ProjectOutputs getProjectOutputs() {
+        return projectOutputs;
+    }
+
+    public void setProjectOutputs(ProjectOutputs projectOutputs) {
+        this.projectOutputs = projectOutputs;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
     }
     
     
---
+++ src/main/java/com/divudi/entity/StakeHolders.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.divudi.entity;
+
+/**
+ *
+ * @author Windhelm
+ */
+class StakeHolders {
+    private String nameOfAgency;
+    private String natureOfIntervention;
+    private String currentStatusOfIntervention;
+    private boolean isIncludeDevelopmentActivity;
+
+    
+    private StakeHolders(String nameOfAgency,String natureOfIntervention, String currentStatusOfIntervention,boolean isIncludeDevelopmentActivity){
+        this.nameOfAgency = nameOfAgency;
+        this.natureOfIntervention = natureOfIntervention;
+        this.currentStatusOfIntervention = currentStatusOfIntervention;
+        this.isIncludeDevelopmentActivity = isIncludeDevelopmentActivity;
+    }
+    /**
+     * @return the nameOfAgency
+     */
+    public String getNameOfAgency() {
+        return nameOfAgency;
+    }
+
+    /**
+     * @param nameOfAgency the nameOfAgency to set
+     */
+    public void setNameOfAgency(String nameOfAgency) {
+        this.nameOfAgency = nameOfAgency;
+    }
+
+    /**
+     * @return the natureOfIntervention
+     */
+    public String getNatureOfIntervention() {
+        return natureOfIntervention;
+    }
+
+    /**
+     * @param natureOfIntervention the natureOfIntervention to set
+     */
+    public void setNatureOfIntervention(String natureOfIntervention) {
+        this.natureOfIntervention = natureOfIntervention;
+    }
+
+    /**
+     * @return the currentStatusOfIntervention
+     */
+    public String getCurrentStatusOfIntervention() {
+        return currentStatusOfIntervention;
+    }
+
+    /**
+     * @param currentStatusOfIntervention the currentStatusOfIntervention to set
+     */
+    public void setCurrentStatusOfIntervention(String currentStatusOfIntervention) {
+        this.currentStatusOfIntervention = currentStatusOfIntervention;
+    }
+
+    /**
+     * @return the isIncludeDevelopmentActivity
+     */
+    public boolean isIsIncludeDevelopmentActivity() {
+        return isIncludeDevelopmentActivity;
+    }
+
+    /**
+     * @param isIncludeDevelopmentActivity the isIncludeDevelopmentActivity to set
+     */
+    public void setIsIncludeDevelopmentActivity(boolean isIncludeDevelopmentActivity) {
+        this.isIncludeDevelopmentActivity = isIncludeDevelopmentActivity;
+    }
+    
+    
+}
